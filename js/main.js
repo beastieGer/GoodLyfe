9564286341a3ea9aaee2115313e56124f895e25d
beastieGer <beastieol@gmail.com>
2024-04-22 14:05:18 +0300

    mobile-menu

--- js/main.js
+++ js/main.js
@@ -6,4 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
 	headerSearchOpen.addEventListener("click", () => {
 		headerSearchForm.classList.toggle("search-form--open");
 	});
+
+	// burger
+	const menuToggle = document.querySelector("#menu-toggle");
+	const mobileMenu = document.querySelector("#header-menu");
+	const bodyEl = document.body;
+	if (menuToggle) {
+		menuToggle.addEventListener("click", () => {
+			if (menuToggle.classList.contains("active")) {
+				menuToggle.classList.remove("active");
+				mobileMenu.classList.remove("active");
+				bodyEl.classList.remove("lock");
+			} else {
+				menuToggle.classList.add("active");
+				mobileMenu.classList.add("active");
+				bodyEl.classList.add("lock");
+			}
+		});
+	}
 });
